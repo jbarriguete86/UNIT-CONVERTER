3721398131bcf11ff22f1649e26574783b972dcc
jbarriguete86 <104594909+jbarriguete86@users.noreply.github.com>
2023-04-12 16:00:46 -0500

    Beautify upload

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const resultContainer = document.querySelectorAll(".convert-txt")
 
 
 
-convertBtn.addEventListener("click", function(){    
+convertBtn.addEventListener("click", function () {
     let mynumber = inputEl.value;
     let darkMode = modeBtn.textContent;
     render(mynumber, darkMode);
@@ -25,12 +25,19 @@ convertBtn.addEventListener("click", function(){
 
 
 function render(number, modes) {
-    const operations = [number * 3.281, number / 3.281, number * 0.264, number / 0.264, number * 2.204, number / 2.204]
+    const operations = [
+        number * 3.281, 
+        number / 3.281, 
+        number * 0.264, 
+        number / 0.264, 
+        number * 2.204, 
+        number / 2.204
+    ]
     let results = []
-    for (let i=0 ; i < operations.length ; i++){
-    results.push(operations[i])// this part of the code is filling the array to avoid changing the DOM many times
+    for (let i = 0; i < operations.length; i++) {
+        results.push(operations[i]) // this part of the code is filling the array to avoid changing the DOM many times
     }
-    overallResults.innerHTML =`
+    overallResults.innerHTML = `
         <div class="convert-txt ${modes === "LIGHT MODE" ? 'dark-subcontainer' : ''}">
             <h3 class ="${modes === "LIGHT MODE" ? 'dark-h3' : ''}">Length(Meter/feet)</h3>
             <p id="length-result" class="${modes === "LIGHT MODE" ? 'dark-p' : ''}">${number} meters = ${results[0].toFixed(3)} feet | ${number} feet = ${results[1].toFixed(3)} meters<p>
@@ -50,22 +57,20 @@ function render(number, modes) {
 
 //LIGHT/DARK MODE BUTTON --- I couldn't manage to get this work properly. It works if nothings is input, but once I have convert the button will not work. I know that it's because of the DOM manipulation, but couldn't manage to figure it out
 
-   modeBtn.addEventListener("dblclick", toggleMode)
+modeBtn.addEventListener("dblclick", toggleMode)
 
-    
-    function toggleMode(){
+
+function toggleMode() {
     overallResults.classList.toggle("dark-container")
     modeBtn.classList.toggle("dark-btn")
-    for(let i = 0; i< paragraphs.length; i++){
+    for (let i = 0; i < paragraphs.length; i++) {
         resultContainer[i].classList.toggle("dark-subcontainer")
         header3[i].classList.toggle("dark-h3")
         paragraphs[i].classList.toggle("dark-p")
-        }
+    }
     if (modeBtn.textContent === "DARK MODE") {
         modeBtn.textContent = "LIGHT MODE"
-        } else {
+    } else {
         modeBtn.textContent = "DARK MODE"
-        }
     }
-    
-    
+}
